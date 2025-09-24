3e2fe8a6b1b8214e4dff1e07b90f798c46af0245
chetan <g.chetankrishna1234@gmail.com>
2025-09-25 00:43:58 +0530

    updated

--- SampleBackend/bookslibrary/src/main/java/com/example/movieslibrary/config/WebConfig.java
+++ SampleBackend/bookslibrary/src/main/java/com/example/movieslibrary/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:2030", "http://localhost:5173", "http://localhost:8080")
+                        .allowedOrigins("http://localhost:5173")   // React port only
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
