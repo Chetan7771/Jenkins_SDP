53dc3cd097e4883442fd131625d545908dab3218
chetan <g.chetankrishna1234@gmail.com>
2025-09-25 11:11:49 +0530

    updated

--- SampleBackend/bookslibrary/src/main/java/com/example/movieslibrary/config/WebConfig.java
+++ SampleBackend/bookslibrary/src/main/java/com/example/movieslibrary/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:2030", "http://localhost:5173","http://localhost:3000") // add your React port
+                        .allowedOrigins("http://localhost:2030", "http://localhost:5173","http://localhost:8081") // add your React port
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
