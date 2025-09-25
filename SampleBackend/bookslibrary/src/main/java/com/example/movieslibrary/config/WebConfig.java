c64d44a9352fdca8400d368d70acaf639a481c8b
chetan <g.chetankrishna1234@gmail.com>
2025-09-25 12:11:14 +0530

    updated

--- SampleBackend/bookslibrary/src/main/java/com/example/movieslibrary/config/WebConfig.java
+++ SampleBackend/bookslibrary/src/main/java/com/example/movieslibrary/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                        .allowedOrigins("http://localhost:2030","http://localhost:5173")
 
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
